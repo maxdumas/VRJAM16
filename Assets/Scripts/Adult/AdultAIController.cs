f0f6679753086d2671765cfcb54db03727063326
Max Dumas <max.dumas@bodylabs.com>
2016-04-16 17:15:03 -0400

    Hook up prefabs better

---
+++ Assets/Scripts/Adult/AdultAIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdultAIController : Controller {
+
+	private Vector3 _target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_target == null) {
+			_target = AcquireTarget;
+		} else {
+
+		}
+	}
+
+	private Vector3 AcquireTarget () {
+		return Vector3.zero;
+	}
+}
