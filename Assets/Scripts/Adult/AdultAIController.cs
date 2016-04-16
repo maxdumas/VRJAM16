751d29c04716fc84832db414b046fa06ebf47c89
Max Dumas <max.dumas@bodylabs.com>
2016-04-16 17:41:59 -0400

    More secripty fixy

--- Assets/Scripts/Adult/AdultAIController.cs
+++ Assets/Scripts/Adult/AdultAIController.cs
@@ -3,23 +3,40 @@ using System.Collections;
 
 public class AdultAIController : Controller {
 
-	private Vector3 _target;
+	private Vector3? _target;
 
+	private AdultMovement _mvmt;
+	private Insect _insect;
+	
 	// Use this for initialization
 	void Start () {
-	
+		_mvmt = GetComponent<AdultMovement> ();
+		_insect = GetComponent<Insect> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (_target == null) {
-			_target = AcquireTarget;
-		} else {
-
+			_target = AcquireTarget();
+		} else if (!_mvmt.MoveTowardsTarget (_target)) {
+			_target = null;
 		}
 	}
 
 	private Vector3 AcquireTarget () {
 		return Vector3.zero;
+	}
+
+	public void OnCollisionEnter (Collision collision) {
+		// If we hit another insect...
+		var otherInsect = collision.gameObject.GetComponent<Insect> ();
+		if (otherInsect != null) {
+			// And it's an adult...
+			if (otherInsect.Stage == Stage.Nymph) {
+				PoolMaster.Despawn(this.gameObject); // We are eaten!
+			} else if (otherInsect.Stage == Stage.Larva) {
+				_insect.Strength += 1;
+			}
+		}
 	}
 }
--- Assets/Scripts/Adult/AdultMovement.cs
+++ Assets/Scripts/Adult/AdultMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class AdultMovement : MonoBehaviour {
+	public float MovementSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +12,21 @@ public class AdultMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public bool MoveTowardsTarget (Vector3? target) {
+		if (!target.HasValue) {
+			return false;
+		}
+		var cv = target.Value - this.transform.position;
+		var direction = cv.normalized;
+		var distance = cv.magnitude;
+		if (distance > MovementSpeed) {
+			transform.position += direction * MovementSpeed;
+		} else {
+			transform.position = Vector3.Lerp (transform.position, target.Value, 0.5f);
+		}
+
+		return (transform.position - target.Value).magnitude > Vector3.kEpsilon;
 	}
 }
