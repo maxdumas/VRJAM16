1b7d66a64fa25d70ba8fdc626742a2195c4c5745
ibrews <alex.coulombe@gmail.com>
2016-04-17 00:21:11 -0400

    that's a night!
    weeeee

--- Assets/Scripts/Nymph/NymphPlayerController.cs
+++ Assets/Scripts/Nymph/NymphPlayerController.cs
@@ -5,28 +5,21 @@ public class NymphPlayerController : MonoBehaviour {
 		
 	public NymphReticle reticle;
 		
-	public float speed = 1.0F;
-	public float startTime;
-	public float journeyLength;
+	public float MaximumSpeed = 50.0F;
 
 	void Start () {
 	}
 	
-	IEnumerator Movement(Vector3 showPos) {
-		startTime = Time.time;
-		journeyLength = Vector3.Distance(transform.position, showPos);
-		float distCovered = (Time.time - startTime) * speed;
-		float fracJourney = distCovered / journeyLength;
-		transform.position = Vector3.Lerp(transform.position, showPos, 0.5f);
-		
-		if (!transform.position.Equals(showPos)) {
+	IEnumerator Movement(Vector3 destination) {
+		while(!transform.position.Equals(destination)) {
+			transform.position = Vector3.MoveTowards(transform.position, destination, MaximumSpeed * Time.deltaTime);
 			yield return null;
 		}
 	}
 
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			StartCoroutine(Movement(reticle.transform.position));
+		if (Input.GetButtonDown ("Fire1") && reticle.TargetIsValid) {
+			StartCoroutine(Movement(reticle.ReticleSpawn.transform.position));
 		}
 	}
 }
--- Assets/Scripts/Nymph/NymphReticle.cs
+++ Assets/Scripts/Nymph/NymphReticle.cs
@@ -5,7 +5,8 @@ public class NymphReticle : MonoBehaviour {
 	public Renderer ReticleSpawn;
 
 	public float DefaultProjectionDistance = 5f;
-	public float ProjectionThreshold = 50f;
+	public float ProjectionThreshold = 20f;
+	public bool TargetIsValid;
 
 	private Color _defaultColor;
 	public Color InvalidTargetColor;
@@ -18,6 +19,12 @@ public class NymphReticle : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (TargetIsValid) {
+			ReticleSpawn.material.color = _defaultColor;
+		} else {
+			ReticleSpawn.material.color = InvalidTargetColor;
+		}
+
 		ReticleSpawn.transform.position = FindReticlePosition ();
 		ReticleSpawn.transform.LookAt (ReticleSpawn.transform.position);
 	}
@@ -29,10 +36,10 @@ public class NymphReticle : MonoBehaviour {
 		
 		Debug.DrawRay(ReticleSpawn.transform.position, fwd, Color.green);
 		if (Physics.Raycast(transform.position, fwd, out hitInfo) && hitInfo.distance < ProjectionThreshold) {
-			ReticleSpawn.material.color = _defaultColor;
+			TargetIsValid = true;
 			return hitInfo.point;
 		} else {
-			ReticleSpawn.material.color = InvalidTargetColor;
+			TargetIsValid = false;
 			return transform.position + fwd * DefaultProjectionDistance;
 		}
 	}
