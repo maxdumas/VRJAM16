af38d32a3430d015a673c2a0c06545e14e6c3b8b
ReluctantPirate <reluctantpirategames@gmail.com>
2016-04-17 11:44:25 -0400

    fixed some sphere prefabs, added basic continue delay

---
+++ Assets/continueDelayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class continueDelayer : MonoBehaviour {
+	
+	public GameObject initialScreen;
+	public GameObject continueScreen;
+
+	public bool isActive;
+	public float delayLength;
+	float timePassed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActive) {
+			timePassed += Time.deltaTime;
+		}
+
+		if (timePassed >= delayLength) {
+			isActive = false;
+			timePassed = 0f;
+			initialScreen.SetActive(false);
+			continueScreen.SetActive(true);
+		}
+	}
+
+	public void exitReset () {
+		initialScreen.SetActive(true);
+		continueScreen.SetActive(false);
+	}
+}
