0c08a43f180351005622e1251bda8b63cdfe004f
ibrews <alex.coulombe@gmail.com>
2016-04-17 12:06:51 -0400

    for Dan's sphere thing
    WireBlack should be able to be black now

---
+++ Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotate : MonoBehaviour {
+
+	public Quaternion randRot;
+	private float minScale = 0.5f;
+	private float maxScale = 1.8f;
+
+
+	void Awake () {
+		//rotVal = gameObject.transform.rotation.y;
+		//rotVal = Random.Range (0f, 360f);
+		randRot = Quaternion.Euler (0, Random.Range (0, 360), 0);
+		transform.rotation = randRot;
+		transform.localScale = Vector3.one * Random.Range(minScale, maxScale);
+
+	}
+	
+
+}
