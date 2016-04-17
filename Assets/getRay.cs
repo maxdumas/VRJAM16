1920f0a7a01c4d49f1bbca4a91c3d1524de39cdc
ReluctantPirate <reluctantpirategames@gmail.com>
2016-04-17 14:44:45 -0400

    made sprites clickable

---
+++ Assets/getRay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+		RaycastHit hit;
+		
+		if (GetComponent<Collider>().Raycast (ray, out hit, 10.0f)) {
+			if (Input.anyKey) {
+				//DO STUFF!
+			}
+		}
+	}
+}
